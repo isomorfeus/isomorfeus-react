2b6bb9684df39c806052eb3d73d7618bbe0b6592
Jan Biedermann <jan@kursator.de>
2018-10-15 00:02:53 +0200

    init component store properly

--- ruby/lib/react/redux_component/reducers.rb
+++ ruby/lib/react/redux_component/reducers.rb
@@ -3,10 +3,14 @@ module React
     module Reducers
       def self.add_component_reducers_to_store
         component_reducer = Redux.create_reducer do |prev_state, action|
-          if action[:type] == 'COMPONENT_STATE'
+          case action[:type]
+          when 'COMPONENT_STATE'
             new_state = {}.merge!(prev_state) # make a copy of state
             new_state[action[:object_id]] = {} unless new_state.has_key?(action[:object_id])
             new_state[action[:object_id]].merge!(action[:name] => action[:value])
+            new_state
+          when 'INIT'
+            new_state = {}
             new_state
           else
             prev_state
@@ -14,10 +18,14 @@ module React
         end
 
         component_class_reducer = Redux.create_reducer do |prev_state, action|
-          if action[:type] == 'COMPONENT_CLASS_STATE'
+          case action[:type]
+          when 'COMPONENT_CLASS_STATE'
             new_state = {}.merge!(prev_state) # make a copy of state
             new_state[action[:class]] = {} unless new_state.has_key?(action[:class])
             new_state[action[:class]].merge!(action[:name] => action[:value])
+            new_state
+          when 'INIT'
+            new_state = {}
             new_state
           else
             prev_state
