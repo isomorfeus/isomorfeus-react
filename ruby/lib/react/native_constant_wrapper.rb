5f3848143cc0bce6dc1abe8afadaa70921a32c98
Jan Biedermann <jan@kursator.de>
2019-05-23 00:03:07 +0200

    Fix native_constant_wrapper, use the correct variable name.

--- ruby/lib/react/native_constant_wrapper.rb
+++ ruby/lib/react/native_constant_wrapper.rb
@@ -10,21 +10,20 @@ module React
     def method_missing(name, *args, &block)
       %x{
         var component = null;
-        if (typeof #@native[name] == "function") {
+        if (typeof #@native[name] === "function") {
           component = #@native[name];
         }
 
         if (component) {
           var children = null;
           var block_result = null;
-          var react_element;
           if (args.length > 0) {
             var last_arg = args[args.length - 1];
             if (typeof last_arg === 'string' || last_arg instanceof String) {
-              if (args.length === 1) { Opal.React.internal_render(react_element, null, last_arg, null); }
-              else { Opal.React.internal_render(react_element, args[0], last_arg, null); }
-            } else { Opal.React.internal_render(react_element, args[0], null, block); }
-          } else { Opal.React.internal_render(react_element, null, null, block); }
+              if (args.length === 1) { Opal.React.internal_render(component, null, last_arg, null); }
+              else { Opal.React.internal_render(component, args[0], last_arg, null); }
+            } else { Opal.React.internal_render(component, args[0], null, block); }
+          } else { Opal.React.internal_render(component, null, null, block); }
         } else {
           #{raise NameError, "No such native Component #@const_name.#{name}"};
         }
