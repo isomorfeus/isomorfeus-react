1e75427eef6dd2d3f313431fd4e9ce5670c33dc3
Jan Biedermann <jan@kursator.de>
2019-05-30 20:09:37 +0200

    Add ReactDOM functionality.

--- ruby/lib/react_dom.rb
+++ ruby/lib/react_dom.rb
@@ -1,31 +1,41 @@
 module ReactDOM
-  def create_portal
+  class << self
+    def create_portal(child, container)
+      # container is a native DOM node
+      `Opal.global.ReactDOM.createPortal(child, container)`
+    end
 
-  end
+    def find_dom_node(native_react_component)
+      `Opal.global.ReactDOM.findDomNode(native_react_component)`
+    end
 
-  def find_dom_node
+    def hydrate(native_react_element, container, &block)
+      # container is a native DOM element
+      `Opal.React.render_buffer.push([]);`
+      result = if block_given?
+                 `Opal.global.ReactDOM.hydrate(native_react_element, container, function() { block.$call() })`
+               else
+                 `Opal.global.ReactDOM.hydrate(native_react_element, container)`
+               end
+      `Opal.React.render_buffer.pop()`
+      result
+    end
 
-  end
+    def render(native_react_element, container, &block)
+      # container is a native DOM element
+      `Opal.React.render_buffer.push([]);`
+      result = if block_given?
+                 `Opal.global.ReactDOM.render(native_react_element, container, function() { block.$call() })`
+               else
+                 `Opal.global.ReactDOM.render(native_react_element, container)`
+               end
+      `Opal.React.render_buffer.pop()`
+      result
+    end
 
-  def hydrate
-
-  end
-
-  def self.render(native_element, container, &block)
-    `Opal.React.render_buffer.push([]);`
-
-    result = if block_given?
-               `ReactDOM.render(native_element, container, function() { block.$call() })`
-             else
-               `ReactDOM.render(native_element, container)`
-             end
-
-    `Opal.React.render_buffer.pop([])`
-
-    result
-  end
-
-  def unmount_component_at_node
-
+    def unmount_component_at_node(container)
+      # container is a native DOM element
+      `Opal.global.ReactDOM.unmountComponentAtNode(container)`
+    end
   end
 end
