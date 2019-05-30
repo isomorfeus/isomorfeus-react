a1eb7dfb0f35b23d7686d1827920f4cdaedc8519
Jan Biedermann <jan@kursator.de>
2019-05-30 20:09:58 +0200

    Add ReactDOMServer functionality.

--- ruby/lib/react_dom_server.rb
+++ ruby/lib/react_dom_server.rb
@@ -1,17 +1,19 @@
 module ReactDOMServer
-  def render_to_string
+  class << self
+    def render_to_string(native_react_element)
+      `Opal.global.ReactDOMServer.renderToString(native_react_element)`
+    end
 
-  end
+    def render_to_static_markup(native_react_element)
+      `Opal.global.ReactDOMServer.renderToStaticMarkup(native_react_element)`
+    end
 
-  def render_to_static_markup
+    def render_to_node_stream(native_react_element)
+      `Opal.global.ReactDOMServer.renderToNodeStream(native_react_element)`
+    end
 
-  end
-
-  def render_to_node_stream
-
-  end
-
-  def render_to_static_node_stream
-
+    def render_to_static_node_stream(native_react_element)
+      `Opal.global.ReactDOMServer.renderToStaticNodeStream(native_react_element)`
+    end
   end
 end
