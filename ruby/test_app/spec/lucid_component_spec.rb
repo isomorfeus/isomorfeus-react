fcb06b8d4ee1c5b18d85b529e00d403bcb6a1bfe
Jan Biedermann <jan@kursator.de>
2019-06-06 00:02:21 +0200

    Add more component tests.

---
+++ ruby/test_app/spec/lucid_component_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+RSpec.describe 'LucidComponent' do
+  it 'can render a component that is using inheritance' do
+    doc = visit('/')
+    doc.evaluate_ruby do
+      class TestComponent < LucidComponent::Base
+        render do
+          DIV(id: :test_component) { 'TestComponent rendered' }
+        end
+      end
+      Isomorfeus::TopLevel.mount_component(TestComponent, {}, '#test_anchor')
+    end
+    node = doc.wait_for('#test_component')
+    expect(node.all_text).to include('TestComponent rendered')
+  end
+
+  it 'can render a component that is using the mixin' do
+    doc = visit('/')
+    doc.evaluate_ruby do
+      class TestComponent
+        include LucidComponent::Mixin
+        render do
+          DIV(id: :test_component) { 'TestComponent rendered' }
+        end
+      end
+      Isomorfeus::TopLevel.mount_component(TestComponent, {}, '#test_anchor')
+    end
+    node = doc.wait_for('#test_component')
+    expect(node.all_text).to include('TestComponent rendered')
+  end
+end
