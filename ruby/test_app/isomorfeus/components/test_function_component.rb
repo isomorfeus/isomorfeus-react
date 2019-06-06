f5acb0e1db02860eb50d4130c75c6bfafb673a8b
Jan Biedermann <jan@kursator.de>
2019-06-06 17:20:18 +0200

    Change to test resolution.

--- ruby/test_app/isomorfeus/components/test_function_component.rb
+++ ruby/test_app/isomorfeus/components/test_function_component.rb
@@ -1,5 +1,14 @@
-class TestFunctionComponent < React::FunctionComponent::Base
+class AFunctionComponent < React::FunctionComponent::Base
+  event_handler do
+    puts 'test'
+  end
   create_function do
     DIV 'test'
+  end
+end
+
+class TestFunctionComponent < React::Component::Base
+  render do
+    AFunctionComponent()
   end
 end
