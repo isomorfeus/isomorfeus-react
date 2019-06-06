64f0aa8ff32d211ca50789797877f682c4497ab7
Jan Biedermann <jan@kursator.de>
2019-06-06 14:56:18 +0200

    Repair React.memo specs.

--- ruby/test_app/spec/react_memo_component_spec.rb
+++ ruby/test_app/spec/react_memo_component_spec.rb
@@ -1,10 +1,6 @@
 require 'spec_helper'
 
 RSpec.describe 'React::MemoComponent' do
-
-  # React.memo as of React 16.8 does not return anything, so it cant be used as top level component,
-  # React complains that it returns nothing. So we use a wrapper component.
-
   it 'can render a component that is using inheritance' do
     doc = visit('/')
     doc.evaluate_ruby do
@@ -13,12 +9,7 @@ RSpec.describe 'React::MemoComponent' do
           DIV(id: :test_component) { 'TestComponent rendered' }
         end
       end
-      class Wrapper < React::Component::Base
-        render do
-          TestComponent()
-        end
-      end
-      Isomorfeus::TopLevel.mount_component(Wrapper, {}, '#test_anchor')
+      Isomorfeus::TopLevel.mount_component(TestComponent, {}, '#test_anchor')
     end
     node = doc.wait_for('#test_component')
     expect(node.all_text).to include('TestComponent rendered')
@@ -33,13 +24,7 @@ RSpec.describe 'React::MemoComponent' do
           DIV(id: :test_component) { 'TestComponent rendered' }
         end
       end
-      class Wrapper
-        include React::Component::Mixin
-        render do
-          TestComponent()
-        end
-      end
-      Isomorfeus::TopLevel.mount_component(Wrapper, {}, '#test_anchor')
+      Isomorfeus::TopLevel.mount_component(TestComponent, {}, '#test_anchor')
     end
     node = doc.wait_for('#test_component')
     expect(node.all_text).to include('TestComponent rendered')
