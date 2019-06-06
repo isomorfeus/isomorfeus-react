5642caf8ea30741286504c8ce250b68e60481ae8
Jan Biedermann <jan@kursator.de>
2019-06-06 17:29:06 +0200

    Simple test_function_component

--- ruby/test_app/isomorfeus/components/test_function_component.rb
+++ ruby/test_app/isomorfeus/components/test_function_component.rb
@@ -1,14 +1,5 @@
-class AFunctionComponent < React::FunctionComponent::Base
-  event_handler do
-    puts 'test'
-  end
+class TestFunctionComponent < React::FunctionComponent::Base
   create_function do
     DIV 'test'
-  end
-end
-
-class TestFunctionComponent < React::Component::Base
-  render do
-    AFunctionComponent()
   end
 end
