4f9232804a0aaa458202e719766b844139a24d29
Jan Biedermann <jan@kursator.de>
2019-06-06 17:19:14 +0200

    Cleanup config.

--- ruby/test_app/webpack/development.js
+++ ruby/test_app/webpack/development.js
@@ -88,14 +88,6 @@ const common_config = {
     },
     // configuration for webpack-dev-server
     devServer: {
-        // uncomment to enable page reload for updates within another directory, which may contain just html files,
-// for example the 'views' directory:
-// before: function(app, server) {
-//     chokidar.watch(path.resolve(__dirname, path.join('..', 'views')).on('all', function () {
-//         server.sockWrite(server.sockets, 'content-changed');
-//     })
-// },
-
         open: false,
         lazy: false,
         port: 3035,
