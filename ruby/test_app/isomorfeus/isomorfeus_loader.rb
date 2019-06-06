369236a5cea435840ca12d5eabf8159426176d16
Jan Biedermann <jan@kursator.de>
2019-06-06 17:19:47 +0200

    Add a native component for testing.

--- ruby/test_app/isomorfeus/isomorfeus_loader.rb
+++ ruby/test_app/isomorfeus/isomorfeus_loader.rb
@@ -4,6 +4,18 @@ require 'isomorfeus-redux'
 require 'isomorfeus-react'
 require 'isomorfeus-react-material-ui'
 
+%x{
+  class NativeComponent extends Opal.global.React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return Opal.global.React.createElement('div', null, 'A');
+    }
+  }
+  Opal.global.NativeComponent = NativeComponent;
+}
+
 require_tree 'components'
 
 Isomorfeus.start_app!
