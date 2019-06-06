b1a2273370dafac1bee39110c86ee09685fd6fe8
Jan Biedermann <jan@kursator.de>
2019-06-06 21:50:17 +0200

    Cleanup Components of test_app

--- ruby/test_app/isomorfeus/components/test_app_app.rb
+++ ruby/test_app/isomorfeus/components/test_app_app.rb
@@ -5,9 +5,6 @@ class TestAppApp < LucidMaterial::App::Base
         Route(path: '/', exact: true, component: HelloComponent.JS[:react_component])
         Route(path: '/ssr', exact: true, component: HelloComponent.JS[:react_component])
         Route(path: '/welcome', exact: true, component: WelcomeComponent.JS[:react_component])
-        Route(path: '/material', exact: true, component: MaterialComponent.JS[:react_component])
-        Route(path: '/function', exact: true, component: TestFunctionComponent.JS[:react_component])
-        Route(path: '/memo', exact: true, component: TestMemoComponent.JS[:react_component])
       end
     end
   end
