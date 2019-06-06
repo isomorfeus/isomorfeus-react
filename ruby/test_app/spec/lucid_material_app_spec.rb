b8c9a5bb39ea6e98b3a8d8f29c79ca6abd333ea9
Jan Biedermann <jan@kursator.de>
2019-06-06 14:55:57 +0200

    LucidMaterial::App specs

---
+++ ruby/test_app/spec/lucid_material_app_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+RSpec.describe 'LucidMaterial::App' do
+  it 'can render a component that is using inheritance' do
+    doc = visit('/')
+    doc.evaluate_ruby do
+      class TestComponent < LucidMaterial::App::Base
+        render do
+          DIV(id: :test_component) { 'TestComponent rendered' }
+        end
+      end
+      Isomorfeus::TopLevel.mount_component(TestComponent, {}, '#test_anchor')
+    end
+    node = doc.wait_for('#test_component')
+    expect(node.all_text).to include('TestComponent rendered')
+  end
+
+  it 'can render a component that is using the mixin' do
+    doc = visit('/')
+    doc.evaluate_ruby do
+      class TestComponent
+        include LucidMaterial::App::Mixin
+        render do
+          DIV(id: :test_component) { 'TestComponent rendered' }
+        end
+      end
+      Isomorfeus::TopLevel.mount_component(TestComponent, {}, '#test_anchor')
+    end
+    node = doc.wait_for('#test_component')
+    expect(node.all_text).to include('TestComponent rendered')
+  end
+end
