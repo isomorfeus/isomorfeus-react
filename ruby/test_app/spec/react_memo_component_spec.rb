92f971636faa022049d8b78b661f081ec7d71a40
Jan Biedermann <jan@kursator.de>
2019-06-07 02:51:39 +0200

    Add specs for props and events for FunctionComponent and MemoComponent

--- ruby/test_app/spec/react_memo_component_spec.rb
+++ ruby/test_app/spec/react_memo_component_spec.rb
@@ -29,4 +29,69 @@ RSpec.describe 'React::MemoComponent' do
     node = doc.wait_for('#test_component')
     expect(node.all_text).to include('TestComponent rendered')
   end
+
+
+  context 'it accepts props and can' do
+    before do
+      @doc = visit('/')
+    end
+
+    it 'access them' do
+      @doc.evaluate_ruby do
+        class TestComponent < React::FunctionComponent::Base
+          create_function do
+            DIV(id: :test_component) do
+              SPAN props.text
+              SPAN props.other_text
+            end
+          end
+        end
+        Isomorfeus::TopLevel.mount_component(TestComponent, { text: 'Prop passed!', other_text: 'Passed other prop!' }, '#test_anchor')
+      end
+      node = @doc.wait_for('#test_component')
+      all_text = node.all_text
+      expect(all_text).to include('Prop passed!')
+      expect(all_text).to include('Passed other prop!')
+    end
+
+    it 'accept a missing prop' do
+      @doc.evaluate_ruby do
+        class TestComponent < React::FunctionComponent::Base
+          create_function do
+            DIV(id: :test_component) { "nothing#{props.a_prop}here" }
+          end
+        end
+        Isomorfeus::TopLevel.mount_component(TestComponent, { }, '#test_anchor')
+      end
+      node = @doc.wait_for('#test_component')
+      expect(node.all_text).to include('nothinghere')
+    end
+  end
+
+  context 'it can handle events like' do
+    before do
+      @doc = visit('/')
+    end
+
+    it 'on_click' do
+      @doc.evaluate_ruby do
+        IT = { clicked: false }
+        class TestComponent < React::FunctionComponent::Base
+          event_handler :change_hash do |event|
+            IT[:clicked] = true
+          end
+          create_function do
+            DIV(id: :test_component, on_click: :change_hash) { 'nothinghere' }
+          end
+        end
+        Isomorfeus::TopLevel.mount_component(TestComponent, { }, '#test_anchor')
+      end
+      node = @doc.wait_for('#test_component')
+      node.click
+      result = @doc.evaluate_ruby do
+        IT[:clicked]
+      end
+      expect(result).to be true
+    end
+  end
 end
