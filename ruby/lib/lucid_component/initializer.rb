b7e0a6ff175c66bf5c14b46cccf92ae215ea5e28
Jan Biedermann <jan@kursator.de>
2019-06-07 02:07:11 +0200

    Use native calls.

--- ruby/lib/lucid_component/initializer.rb
+++ ruby/lib/lucid_component/initializer.rb
@@ -2,11 +2,11 @@ module LucidComponent
   module Initializer
     def initialize(native_component)
       @native = native_component
-      @app_store = ::React::ReduxComponent::AppStoreProxy.new(self, 'props')
-      @class_store = ::React::ReduxComponent::ClassStoreProxy.new(self, 'props')
+      @app_store = `Opal.React.ReduxComponent.AppStoreProxy.$new(#{self}, 'props')`
+      @class_store = `Opal.React.ReduxComponent.ClassStoreProxy.$new(#{self}, 'props')`
       @props = `Opal.React.Component.Props.$new(#@native.props)`
       @state = `Opal.React.Component.State.$new(#@native)`
-      @store = ::React::ReduxComponent::InstanceStoreProxy.new(self, 'props')
+      @store = `Opal.React.ReduxComponent.InstanceStoreProxy.$new(#{self}, 'props')`
     end
   end
 end
--- ruby/lib/react/redux_component/initializer.rb
+++ ruby/lib/react/redux_component/initializer.rb
@@ -3,11 +3,11 @@ module React
     module Initializer
       def initialize(native_component)
         @native = native_component
-        @app_store = ::React::ReduxComponent::AppStoreProxy.new(self)
-        @class_store = ::React::ReduxComponent::ClassStoreProxy.new(self)
+        @app_store = `Opal.React.ReduxComponent.AppStoreProxy.$new(#{self})`
+        @class_store = `Opal.React.ReduxComponent.ClassStoreProxy.$new(#{self})`
         @props = `Opal.React.Component.Props.$new(#@native.props)`
         @state = `Opal.React.Component.State.$new(#@native)`
-        @store = ::React::ReduxComponent::InstanceStoreProxy.new(self)
+        @store = `Opal.React.ReduxComponent.InstanceStoreProxy.$new(#{self})`
       end
     end
   end
