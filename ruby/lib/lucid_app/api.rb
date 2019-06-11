2636713fcce1f28b0ce07e3e5cc28c749e1382bf
Jan Biedermann <jan@kursator.de>
2019-06-11 23:46:01 +0200

    Remove duplicate code.

--- ruby/lib/lucid_app/api.rb
+++ ruby/lib/lucid_app/api.rb
@@ -1,12 +1,6 @@
 module LucidApp
   module API
     def self.included(base)
-      base.instance_exec do
-        def render(&block)
-          `base.render_block = block`
-        end
-      end
-
       def context
         @native.JS[:context]
       end
