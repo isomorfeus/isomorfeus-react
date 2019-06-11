60947fecb945f6e63fa1e52abb412d49b5211e6d
Jan Biedermann <jan@kursator.de>
2019-06-11 20:42:41 +0200

    has_key? -> key?

--- ruby/lib/react/redux_component/class_store_proxy.rb
+++ ruby/lib/react/redux_component/class_store_proxy.rb
@@ -21,7 +21,7 @@ module React
           if @native_component_instance.JS[@access_key].JS[:isomorfeus_store].JS[:component_class_state].JS[@component_name] &&
             @native_component_instance.JS[@access_key].JS[:isomorfeus_store].JS[:component_class_state].JS[@component_name].JS.hasOwnProperty(key)
             return @native_component_instance.JS[@access_key].JS[:isomorfeus_store].JS[:component_class_state].JS[@component_name].JS[key]
-          elsif @component_instance.class.default_class_store_defined && @component_instance.class.class_store.to_h.has_key?(key)
+          elsif @component_instance.class.default_class_store_defined && @component_instance.class.class_store.to_h.key?(key)
             # check if a default value was given
             return @component_instance.class.class_store.to_h[key]
           end
