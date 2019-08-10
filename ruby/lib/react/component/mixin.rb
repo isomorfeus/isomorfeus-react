b8279922fcc96479f5f201e0c2ddea910d996fa4
Jan Biedermann <jan@kursator.de>
2019-08-10 12:30:30 +0200

    Don't load react/component/unsafe_api by default.

--- ruby/lib/react/component/mixin.rb
+++ ruby/lib/react/component/mixin.rb
@@ -10,7 +10,7 @@ module React
         base.include(::React::Component::Elements)
         base.include(::React::Component::API)
         base.include(::React::Component::Callbacks)
-        base.include(::React::Component::UnsafeAPI)
+        # base.include(::React::Component::UnsafeAPI)
         base.include(::React::Component::Initializer)
         base.include(::React::Component::Features)
         base.include(::React::Component::Resolution)
