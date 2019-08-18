8be26964cfff3a0b05cf33ac7a056b3c6eea771c
Jan Biedermann <jan@kursator.de>
2019-08-18 16:02:22 +0200

    Fix location host in view helper.

--- ruby/lib/isomorfeus/react_view_helper.rb
+++ ruby/lib/isomorfeus/react_view_helper.rb
@@ -19,12 +19,13 @@ module Isomorfeus
 
         # if location_host and scheme are given and if Transport is loaded, connect and then render, otherwise do not render
         ws_scheme = props[:location_scheme] == 'https:' ? 'wss:' : 'ws:'
+        location_host = props[:location_host] ? props[:location_host] : 'localhost'
         api_ws_path = Isomorfeus.respond_to?(:api_websocket_path) ? Isomorfeus.api_websocket_path : ''
         javascript << <<~JAVASCRIPT
-          var location_host = '#{props[:location_host]}';
+          var location_host = '#{location_host}';
           var ws_scheme = '#{ws_scheme}';
           var api_ws_path = '#{api_ws_path}';
-          if (typeof global.Opal.Isomorfeus.Transport !== 'undefined' && location_host !== '' && api_ws_path !== '') {
+          if (typeof global.Opal.Isomorfeus.Transport !== 'undefined' && api_ws_path !== '') {
             global.Opal.Isomorfeus.TopLevel["$transport_ws_url="](ws_scheme + location_host + api_ws_path);
             global.Opal.send(global.Opal.Isomorfeus.Transport.$promise_connect(), 'then', [], ($$1 = function(){
               try {
