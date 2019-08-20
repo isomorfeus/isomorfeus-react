643054f58c0f61d304b6c9c766acb6416372cbac
Jan Biedermann <jan@kursator.de>
2019-08-21 01:33:59 +0200

    support passing theme to block.

--- ruby/lib/lucid_material/component/api.rb
+++ ruby/lib/lucid_material/component/api.rb
@@ -4,9 +4,17 @@ module LucidMaterial
       def self.included(base)
         base.instance_exec do
           def styles(styles_hash = nil, &block)
-            styles_hash = block.call if block_given?
-            `base.jss_styles = #{styles_hash.to_n}` if styles_hash
-            `Opal.Hash.$new(base.jss_styles)`
+            if block_given?
+              %x{
+                base.jss_styles = function(theme) {
+                  var result = block.$call(Opal.Hash.$new(theme));
+                  return result.$to_n();
+                }
+              }
+            else
+              `base.jss_styles = #{styles_hash.to_n}` if styles_hash
+              `Opal.Hash.$new(base.jss_styles)`
+            end
           end
         end
 
