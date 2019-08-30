ee02e80cfc36f013e3671545f64157041233f021
Jan Biedermann <jan@kursator.de>
2019-08-30 09:52:05 +0200

    Fix bug: props.(match|location|history) not updating

--- ruby/lib/react/component/props.rb
+++ ruby/lib/react/component/props.rb
@@ -25,30 +25,27 @@ module React
 
       # for router convenience
       def history
-        return @history if @history
         return nil unless @native.JS[:props].JS[:history]
         if @native.JS[:props].JS[:history].JS[:pathname]
-          @history = React::Component::History.new(@native.JS[:props].JS[:history])
+          React::Component::History.new(@native.JS[:props].JS[:history])
         else
           @native.JS[:props].JS[:history]
         end
       end
 
       def location
-        return @location if @location
         return nil unless @native.JS[:props].JS[:location]
         if @native.JS[:props].JS[:location].JS[:pathname]
-          @location = React::Component::Location.new(@native.JS[:props].JS[:location])
+          React::Component::Location.new(@native.JS[:props].JS[:location])
         else
           @native.JS[:props].JS[:location]
         end
       end
 
       def match
-        return @match if @match
         return nil unless @native.JS[:props].JS[:match]
         if @native.JS[:props].JS[:match].JS[:path]
-          @match = React::Component::Match.new(@native.JS[:props].JS[:match])
+          React::Component::Match.new(@native.JS[:props].JS[:match])
         else
           @native.JS[:props].JS[:match]
         end
