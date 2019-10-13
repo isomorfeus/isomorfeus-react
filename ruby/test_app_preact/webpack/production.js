6dfe37b5f1b6d042bf955adfd32049e4325bc258
Jan Biedermann <jan@kursator.de>
2019-10-13 08:44:39 +0200

    Use preact SSR

--- ruby/test_app_preact/webpack/production.js
+++ ruby/test_app_preact/webpack/production.js
@@ -96,18 +96,18 @@ const ssr_config = {
     ],
 };
 
-const web_worker_config = {
-    target: 'webworker',
-    entry: {
-        web_worker: [path.resolve(__dirname, '../isomorfeus/imports/application_web_worker.js')]
-    },
-    plugins: [
-        new WebpackAssetsManifest({ publicPath: true, merge: true }) // generate manifest
-    ],
-};
+// const web_worker_config = {
+//     target: 'webworker',
+//     entry: {
+//         web_worker: [path.resolve(__dirname, '../isomorfeus/imports/application_web_worker.js')]
+//     },
+//     plugins: [
+//         new WebpackAssetsManifest({ publicPath: true, merge: true }) // generate manifest
+//     ],
+// };
 
 const browser = Object.assign({}, common_config, browser_config);
 const ssr = Object.assign({}, common_config, ssr_config);
-const web_worker = Object.assign({}, common_config, web_worker_config);
+// const web_worker = Object.assign({}, common_config, web_worker_config);
 
 module.exports = [ browser, ssr ];
