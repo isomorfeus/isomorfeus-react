c70679e91d40b80f09149df8143f3adaba05ab93
Jan Biedermann <jan@kursator.de>
2019-10-18 20:31:00 +0200

    Fix and ensure execution of preload_block in instance.

--- ruby/lib/lucid_component/preloading_support.rb
+++ ruby/lib/lucid_component/preloading_support.rb
@@ -1,20 +1,16 @@
 module LucidComponent
   module PreloadingSupport
-    def self.included(base)
-      base.instance_exec do
-        def preload(&block)
-          `base.preload_block = block`
-          component_did_mount do
-            `base.preload_block`.call.then do
-              set_state({preloaded: true})
-            end
-          end
-        end
-
-        def while_loading(&block)
-          `base.while_loading_block = block`
+    def preload(&block)
+      `self.preload_block = block`
+      component_did_mount do
+        instance_exec(&self.class().JS[:preload_block]).then do
+          set_state({preloaded: true})
         end
       end
+    end
+
+    def while_loading(&block)
+      `self.while_loading_block = block`
     end
   end
 end
