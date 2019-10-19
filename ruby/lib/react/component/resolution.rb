5ab5394338394177ffa7803e61015129545d9416
Jan Biedermann <jan@kursator.de>
2019-10-19 22:05:09 +0200

    Support Component resolution when lazy loading is combined with auto loading.

--- ruby/lib/react/component/resolution.rb
+++ ruby/lib/react/component/resolution.rb
@@ -3,7 +3,7 @@ module React
     module Resolution
       def self.included(base)
         base.instance_exec do
-          alias _react_component_resolution_original_const_missing const_missing
+          alias _react_component_class_resolution_original_const_missing const_missing
 
           def const_missing(const_name)
             # language=JS
@@ -13,7 +13,47 @@ module React
                 #{Object.const_set(const_name, `new_const`)};
                 return new_const;
               } else {
-                return #{_react_component_resolution_original_const_missing(const_name)};
+                return #{_react_component_class_resolution_original_const_missing(const_name)};
+              }
+            }
+          end
+
+          # this is required for autoloading support, as the component may not be loaded and so its method is not registered.
+          # must load it first, done by const_get, and next time the method will be there.
+          alias _react_component_class_resolution_original_method_missing method_missing
+
+          def method_missing(component_name, *args, &block)
+            # check for ruby component and render it
+            # otherwise pass on method missing
+            # language=JS
+            %x{
+              var modules = self.$to_s().split("::");
+              var modules_length = modules.length;
+              var module;
+              var constant;
+              var component;
+              for (var i = modules_length; i > 0; i--) {
+                try {
+                  module = modules.slice(0, i).join('::')
+                  constant = self.$const_get(module).$const_get(component_name, false);
+                  if (typeof constant.react_component !== 'undefined') {
+                    component = constant.react_component;
+                    break;
+                  }
+                } catch(err) { component = null; }
+              }
+              if (!component) {
+                try {
+                  constant = Opal.Object.$const_get(component_name);
+                  if (typeof constant.react_component !== 'undefined') {
+                    component = constant.react_component;
+                  }
+                } catch(err) { component = null; }
+              }
+              if (component) {
+                return Opal.React.internal_prepare_args_and_render(component, args, block);
+              } else {
+                return #{_react_component_resolution_original_method_missing(component_name, *args, block)};
               }
             }
           end
