04f9243e28a665c2a243e1b763015d1dc9ad852b
Jan Biedermann <jan@kursator.de>
2019-10-22 01:50:32 +0200

    Prevent error.

--- ruby/test_app_react/isomorfeus/components/page_404_component.rb
+++ ruby/test_app_react/isomorfeus/components/page_404_component.rb
@@ -1,4 +1,4 @@
-class Page404Component < LucidComponent::Base
+class Page404Component < LucidMaterial::Component::Base
   render do
     Isomorfeus.ssr_response_status = 404
     P "Oops, page not found!"
