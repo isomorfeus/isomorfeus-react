2212cbc61b74b0a3b88dbfd19a9c12fb4e580198
Jan Biedermann <jan@kursator.de>
2020-03-13 05:21:55 +0100

    Refactor component resolution, fixing bug, call native components from wrapper.

--- ruby/lib/react/component/resolution.rb
+++ ruby/lib/react/component/resolution.rb
@@ -6,10 +6,10 @@ module React
           alias _react_component_class_resolution_original_const_missing const_missing
 
           def const_missing(const_name)
-            # language=JS
             %x{
               if (typeof Opal.global[const_name] !== "undefined" && (const_name[0] === const_name[0].toUpperCase())) {
                 var new_const = #{React::NativeConstantWrapper.new(`Opal.global[const_name]`, const_name)};
+                new_const.react_component = Opal.global[const_name];
                 #{Object.const_set(const_name, `new_const`)};
                 return new_const;
               } else {
@@ -20,7 +20,6 @@ module React
 
           # this is required for autoloading support, as the component may not be loaded and so its method is not registered.
           # must load it first, done by const_get, and next time the method will be there.
-
           unless method_defined?(:_react_component_class_resolution_original_method_missing)
             alias _react_component_class_resolution_original_method_missing method_missing
           end
@@ -28,36 +27,21 @@ module React
           def method_missing(component_name, *args, &block)
             # check for ruby component and render it
             # otherwise pass on method missing
-            # language=JS
             %x{
-              var modules = self.$to_s().split("::");
-              var modules_length = modules.length;
-              var module;
               var constant;
-              var component;
-              for (var i = modules_length; i > 0; i--) {
-                try {
-                  module = modules.slice(0, i).join('::');
-                  constant = self.$const_get(module).$const_get(component_name, false);
-                  if (typeof constant.react_component !== 'undefined') {
-                    component = constant.react_component;
-                    break;
-                  }
-                } catch(err) { component = null; }
-              }
-              if (!component) {
-                try {
-                  constant = Opal.Object.$const_get(component_name);
-                  if (typeof constant.react_component !== 'undefined') {
-                    component = constant.react_component;
-                  }
-                } catch(err) { component = null; }
-              }
-              if (component) {
-                return Opal.React.internal_prepare_args_and_render(component, args, block);
-              } else {
-                return #{_react_component_class_resolution_original_method_missing(component_name, *args, block)};
-              }
+              if (typeof self.iso_react_const_cache === 'undefined') { self.iso_react_const_cache = {}; }
+              try {
+                if (typeof self.iso_react_const_cache[component_name] !== 'undefined') {
+                  constant = self.iso_react_const_cache[component_name]
+                } else {
+                  constant = self.$const_get(component_name);
+                  self.iso_react_const_cache[component_name] = constant;
+                }
+                if (typeof constant.react_component !== 'undefined') {
+                  return Opal.React.internal_prepare_args_and_render(constant.react_component, args, block);
+                }
+              } catch(err) { }
+              return #{_react_component_class_resolution_original_method_missing(component_name, *args, block)};
             }
           end
         end
@@ -72,40 +56,24 @@ module React
         # first check for native component and render it, we want to be fast for native components
         # second check for ruby component and render it, they are a bit slower anyway
         # third pass on method missing
-        # language=JS
         %x{
-          var component = null;
-          if (typeof Opal.global[component_name] !== "undefined" && (component_name[0] === component_name[0].toUpperCase())) {
-            component = Opal.global[component_name];
-          } else {
-            var modules = self.$to_s().split("::");
-            var modules_length = modules.length;
-            var module;
-            var constant;
-            for (var i = modules_length; i > 0; i--) {
-              try {
-                module = modules.slice(0, i).join('::');
-                constant = self.$class().$const_get(module).$const_get(component_name, false);
-                if (typeof constant.react_component !== 'undefined') {
-                  component = constant.react_component;
-                  break;
-                }
-              } catch(err) { component = null; }
+          var constant;
+          if (typeof self.iso_react_const_cache === 'undefined') { self.iso_react_const_cache = {}; }
+          try {
+            if (typeof self.iso_react_const_cache[component_name] !== 'undefined') {
+              constant = self.iso_react_const_cache[component_name]
+            } else if (typeof self.$$is_module !== 'undefined') {
+              constant = self.$const_get(component_name);
+              self.iso_react_const_cache[component_name] = constant;
+            } else {
+              constant = self.$class().$const_get(component_name);
+              self.iso_react_const_cache[component_name] = constant;
             }
-            if (!component) {
-              try {
-                constant = Opal.Object.$const_get(component_name);
-                if (typeof constant.react_component !== 'undefined') {
-                  component = constant.react_component;
-                }
-              } catch(err) { component = null; }
+            if (typeof constant.react_component !== 'undefined') {
+              return Opal.React.internal_prepare_args_and_render(constant.react_component, args, block);
             }
-          }
-          if (component) {
-            return Opal.React.internal_prepare_args_and_render(component, args, block);
-          } else {
-            return #{_react_component_resolution_original_method_missing(component_name, *args, block)};
-          }
+          } catch(err) { }
+          return #{_react_component_resolution_original_method_missing(component_name, *args, block)};
         }
       end
     end
