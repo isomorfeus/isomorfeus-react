39390e610fdbf9ecd1e08f4f9d8f2c9b16c5c575
Jan Biedermann <jan@kursator.de>
2020-03-30 00:09:07 +0200

    Increase allowed asset size a bit

--- ruby/common_spec/asset_size_spec.rb
+++ ruby/common_spec/asset_size_spec.rb
@@ -30,7 +30,7 @@ RSpec.describe 'Asset sizes' do
       parsed_size = obj['parsedSize'] / 1024
       gzip_size = obj['gzipSize'] / 1024
       puts "Max asset sizes: #{lib_key}: stat: #{stat_size}kb, parsed: #{parsed_size}kb, gzip: #{gzip_size}kb"
-      expect(stat_size < 350).to be true
+      expect(stat_size < 351).to be true
       expect(parsed_size < 200).to be true
       expect(gzip_size < 30).to be true
     end
