9dc96b9c567bded0d4ed2c6f7f9ba4be020c6333
Jan Biedermann <jan@kursator.de>
2021-07-30 06:37:06 +0200

    use chrome and iodine

--- ruby/common_spec/spec_helper.rb
+++ ruby/common_spec/spec_helper.rb
@@ -9,8 +9,8 @@ require_relative '../test_app_app'
 ASSETS_COMPILED ||= system('yarn run production_build')
 
 Isomorfeus::Puppetmaster.download_path = File.join(Dir.pwd, 'download_path_tmp')
-Isomorfeus::Puppetmaster.driver = :chromium
-Isomorfeus::Puppetmaster.server = :puma
+Isomorfeus::Puppetmaster.driver = :chrome
+Isomorfeus::Puppetmaster.server = :iodine
 Isomorfeus::Puppetmaster.app = TestAppApp
 Isomorfeus::Puppetmaster.boot_app
 
